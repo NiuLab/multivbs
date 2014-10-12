18f493b59c378f7b3f00859b2f6b2be25c221e09
Charlie Brown <stieizc@gmail.com>
2014-10-12 11:26:02 +0800

    config: add parser

--- libvbs/config.py
+++ libvbs/config.py
@@ -1,5 +1,23 @@
-from yaml import load, dump
+import yaml
 try:
     from yaml import Cloader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
+
+class Config:
+    def __init__(self, path):
+        """VBS configuration parser"""
+        self.general = {}
+        self.bts_lists = []
+        self.filepath = path
+        self.read_file()
+
+    def read_file(self):
+        """Read contents from self.filepath"""
+        with open(self.filepath, 'r') as config_file:
+            section = yaml.load(config_file, Loader=Loader)
+            self.general = section['general']
+            self.bts_lists = section['bts_lists']
+
+    def __str__(self):
+        return "General settings:\n{0}\nBTS Lists:\n{1}".format(self.general, self.bts_lists)
