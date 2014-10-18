39c96ff236a9be070a7cdab60afb2e9abdc1d796
Charlie Brown <stieizc@gmail.com>
2014-10-18 14:42:50 +0800

    change tests

--- test/configs.py
+++ test/configs.py
@@ -2,7 +2,12 @@ import sys
 sys.path.append("..")
 del sys
 from libvbs.config import Config
+from libvbs.container import *
 
 def run():
     config = Config("vbs.yaml")
     print(config)
+    corenetwork = CoreNetwork(config.general)
+    print(corenetwork)
+    bts = BaseStation(config.general, config.bts_list[0])
+    print(bts)
